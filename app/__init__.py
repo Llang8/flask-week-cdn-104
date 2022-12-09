6c170aa5f5db3d61e5b3e89c187e2b24b3ee1453
Lucas Lang <32493084+Llang8@users.noreply.github.com>
2022-12-09 09:57:14 -0600

    Testing

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,10 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 
+""" 
+IS THIS WORKING?
+"""
+
 db = SQLAlchemy()
 migrate = Migrate()
 login_manager = LoginManager()
